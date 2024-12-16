732f202f0ce930a07f6b4a7f974de66c3ca57bbf
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2024-12-17 00:16:35 +0900

    基本ページ完了

--- udemy-household-app/src/App.tsx
+++ udemy-household-app/src/App.tsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Report from './pages/Report';
+import NoMatch from './pages/NoMatch';
 
 function App() {
 
   return (
     <>
         <h1>hello React!</h1>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/report" element={<Report />}/>
+            <Route path="/*" element={<NoMatch />}/>
+          </Routes>
+        </Router>
     </>
   )
 }
