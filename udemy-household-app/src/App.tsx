8694201bfd53356c16d6d8607cb32a35563d624e
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2024-12-20 00:35:26 +0900

    フォント設定

--- udemy-household-app/src/App.tsx
+++ udemy-household-app/src/App.tsx
@@ -8,11 +8,16 @@ import Home from './pages/Home';
 import Report from './pages/Report';
 import NoMatch from './pages/NoMatch';
 import AppLayout from './components/layout/AppLayout';
+import {theme} from './theme/theme';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 
 function App() {
 
   return (
     <>
+        <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Router>
           <Routes>
             <Route path="/" element={<AppLayout />}>
@@ -22,6 +27,7 @@ function App() {
             </Route>
           </Routes>
         </Router>
+        </ThemeProvider>
     </>
   )
 }
